f0b182339121f9c0eb1b28f565cf526302d265ce
Aime Cesaire <cmugishawayo25@amherst.edu>
2022-04-20 04:46:46 +0000

    Progress

--- mmu.c
+++ mmu.c
@@ -46,6 +46,39 @@ mmu_init (vmsim_addr_t new_upper_pt_addr) {
 vmsim_addr_t
 mmu_translate (vmsim_addr_t sim_addr) {
 
+  // decompose the address
+  uint32_t offset = sim_addr & 0xfff;
+  uint32_t upper_10_bits = sim_addr & 0x3ff000;
+  uint32_t lower_10_bits = sim_addr & 0xffc00000;
+
+  uint32_t upt_index = upper_10_bits >> 22 ; //cut off the lower 22 zeros
+
+  //test the first condition
+  while (upper_pt_addr[upt_index] == 0){
+    vmsim_map_fault(); // i should try recursion if with recursion if this fails **
+  }
+  
+  uint32_t lpt_index = lower_10_bits >> 12 ; // cut off the lower 12 bits
+
+  vmsim_addr_t lower_pt_addr = upper_pt_addr[upt_index]; //get the base address of the lpt
+
+  while (upper_pt_addr[upt_index] != 0 && lower_pt_addr[lpt_index] == 0){
+    vmsim_map_fault(); //** if with recursion maybe ?
+  }
+
+  if (upper_pt_addr[upt_index] != 0 && lower_pt_addr[lpt_index] != 0){
+    vmsim_addr_t real_page_addr = (vmsim_addr_t) lower_pt_addr[lpt_index]; // the real page should be here
+
+    uint32_t real_page_temp = (uint32_t) real_page_addr; // make a copy to not screw up things
+    real_page_temp = real_page_temp << 12 ; //make place for the offset , we have 12 zeros tailing the number
+    real_page_temp = real_page_temp | 0xfff; //flip all the 12 0s at tail to 1s
+    real_page_temp = real_page_temp & offset ; // copy the offset into place
+
+    vmsim_addr_t real_address = (vmsim_addr_t) real_page_temp;
+
+    return real_address;
+  }
+  
   // REPLACE THIS WITH YOUR OWN, FULL IMPLEMENTATION OF THIS FUNCTION.
   return 0;
   
