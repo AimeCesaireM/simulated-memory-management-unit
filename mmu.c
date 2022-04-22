f0f6c526e6d346b0cf6fe88481a6aef796dced84
Aime Cesaire <cmugishawayo25@amherst.edu>
2022-04-22 04:22:09 +0000

    Final code

--- mmu.c
+++ mmu.c
@@ -61,7 +61,7 @@ mmu_translate (vmsim_addr_t sim_addr) {
   pt_entry_t upt_entry;
   vmsim_read_real(&upt_entry, (vmsim_addr_t) upper_pt_addr + upt_index, sizeof(uint32_t)); // read the contents of the upt[index] into upt_entry
 
-  if (upper_pt_addr + upt_index == 0){
+  if (upt_entry == 0){
     vmsim_map_fault(sim_addr);
     mmu_translate(sim_addr);
   }
@@ -70,20 +70,18 @@ mmu_translate (vmsim_addr_t sim_addr) {
   lpt_index = lpt_index * 4;
   printf("LPT index: %d \n", lpt_index);
 
-		  vmsim_addr_t lower_pt_addr = (vmsim_addr_t) upt_entry; //get the base address of the lpt
-  //printf("lower page table address: %x \n", lower_pt_addr);
-   
+   vmsim_addr_t lower_pt_addr = (vmsim_addr_t) upt_entry; //get the base address of the lpt
+  //printf("lower page table base address: %x \n", lower_pt_addr);
 
-   if (upper_pt_addr + upt_index != 0 && lower_pt_addr + lpt_index == 0){
+   pt_entry_t lpt_entry;
+    vmsim_read_real(&lpt_entry, (vmsim_addr_t) lower_pt_addr + lpt_index, sizeof(uint32_t)); // read the contents at the location
+
+   if (upt_entry != 0 && lpt_entry == 0){
     vmsim_map_fault(sim_addr);
     mmu_translate(sim_addr);
-}
-
-  //if (upper_pt_addr + upt_index != 0 && lower_pt_addr + lpt_index != 0)
-    pt_entry_t lpt_entry;
-		  vmsim_read_real(&lpt_entry, (vmsim_addr_t) lower_pt_addr + lpt_index, sizeof(uint32_t));
-		  
-    vmsim_addr_t real_page_addr = (vmsim_addr_t) lpt_entry + lpt_index; // the real page should be here
+   }
+  
+    vmsim_addr_t real_page_addr = (vmsim_addr_t) lpt_entry; // the real page should be here
     //printf(" real page address: %x \n", real_page_addr);
             
     vmsim_addr_t real_address = (vmsim_addr_t) real_page_addr + offset;
